43994ce2120be34216e76b9eb1ea5ca121ebe778
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-05-19 16:38:02 +0200

    Fixx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [feedback, setFeedback] = useState(() => {
+    const saveFeedback = localStorage.getItem("feedback");
+    return saveFeedback
+      ? JSON.parse(saveFeedback)
+      : {
+          good: 0,
+          neutral: 0,
+          bad: 0,
+        };
+  });
+  const total = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback =
+    total > 0 ? Math.round((feedback.good / total) * 100) : 0;
+
+  const updateFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
+  const reset = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React+amazzing</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        reset={reset}
+        totalFeedback={total}
+      />
+      {total > 0 ? (
+        <Feedback
+          feedback={feedback}
+          total={total}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
   );
 }
 
